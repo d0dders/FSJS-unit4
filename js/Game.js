87ab9d44625ab77f52c9f819bf42a5d7a29236a3
d0dders <dodders@outlook.com>
2021-03-29 11:47:07 +0100

    Display gameover overlay, and reset board if playing again.

--- js/Game.js
+++ js/Game.js
@@ -2,6 +2,9 @@
  * Project 4 - OOP Game App
  * Game.js */
 
+const overlay = document.getElementById('overlay');
+const lives = document.querySelectorAll('.tries>img');
+
 class Game {
     constructor() {
         this.missed = 0;
@@ -14,7 +17,6 @@ class Game {
 
 
     startGame() {
-        const overlay = document.getElementById('overlay');
         overlay.style.display = 'none';
         this.activePhrase = new Phrase(this.getRandomPhrase());
         this.activePhrase.addPhraseToDisplay();
@@ -40,28 +42,51 @@ class Game {
             letterButton.classList.add('chosen');
             this.activePhrase.showMatchedLetter(letter);
             if (this.checkForWin()){
-                console.log('YOU WON');
-                //gameover WIN
-
+                this.gameOver(true);
             }
         }
     }
 
 
     removeLife() {
-        const lives = document.querySelectorAll('.tries>img');
         lives[this.missed].setAttribute('src', 'images/lostHeart.png');
         this.missed++;
         if (this.missed === 5) {
             console.log('GAME OVER');
-            //gameOver()
+            this.gameOver(false);
         }
     }
 
 
     checkForWin() {
         const letters = [...document.querySelectorAll('.letter')];
-        console.log(letters);
         return letters.every(letter => letter.classList.contains('show'));
+    }
+
+
+    gameOver(gameWon) {
+        const gameOverText = document.getElementById('game-over-message');
+        overlay.classList.remove('start');
+        if (gameWon){
+            gameOverText.innerText = 'Congratulations! You won!';
+            overlay.classList.add('win');
+        } else {
+            overlay.classList.add('lose');
+            gameOverText.innerText = 'Bummer! Try again!';
+        }
+        overlay.style.display = 'flex';
+        this.reset();
+    }
+
+
+    reset() {
+        const keys = document.querySelectorAll('.key');
+        keys.forEach(key => { 
+            key.className = 'key';
+            key.disabled = false;
+        });
+        const phraseList = document.querySelector('#phrase>ul');
+        phraseList.innerHTML = '';
+        lives.forEach(life => life.setAttribute('src', 'images/liveHeart.png'));
     }
 }
